435e27ae03ceed516476805e0780c978cd6d629f
Manuel David Peñaranda Vela <mpenvel@gmail.com>
2022-01-15 17:34:27 +0100

    Initial commit

---
+++ src/main/java/com/rindus/codingtask/CodingTaskApplication.java
@@ -0,0 +1,13 @@
+package com.rindus.codingtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rindus/codingtask/CodingTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rindus.codingtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
